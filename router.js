3c8a0a13f85af8b2545da365095883bcb61cc28a
Green88 <taniagr0808@gmail.com>
2016-08-20 16:52:39 +0300

    base book routes fixes

--- router.js
+++ router.js
@@ -2,67 +2,8 @@
  * Created by Tania on 07/08/16.
  */
 
-var Book = require('./dbModels/BookModel');
-var uuid = require('node-uuid');
+var booksRoutes = require('./routes/books');
 
-/**
- * @param {App} app
- */
 module.exports = function(app) {
-    app.get('/books', getAllBooks);
-
-    app.get('/books/:bookId', getBookById);
-
-    app.post('/book', saveBookToDB);
+    booksRoutes(app);
 };
-
-function getAllBooks(req, res, next) {
-    Book.find({}, function(err, result) {
-        if(err) {
-            console.log(err);
-            return next(err);
-        }
-
-        res.send({result: 'ok', payload: result});
-    });
-}
-
-function getBookById(req, res, next) {
-    Book.findOne({bookId: req.params.bookId}, function(err, result) {
-        if(err) {
-            console.log(err);
-            return next(err);
-        }
-
-        res.send({result: 'ok', payload: result});
-    });
-}
-
-function saveBookToDB(req, res, next) {
-    var book = new Book({
-        bookId: req.body.bookId,
-        title: req.body.title,
-        description: req.body.description,
-        imageUrls: req.body.imageUrls,
-        text: req.body.text,
-        genre: req.body.genre,
-        finished: req.body.finished,
-        rating: req.body.rating,
-        author: {
-            authorId: req.body.authorId,
-            name: req.body.authorName,
-            imageUrl: req.body.authorImageUrl
-        }
-    });
-
-
-    book.save(function(err) {
-        if (err) {
-            console.log(err);
-            return next(err);
-        }
-
-        // Repond to request indicating the book was created
-        res.json({ payload: book});
-    });
-}
