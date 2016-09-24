b06e0748e4ef4729597de8025ad2da3dee339bc9
Green88 <taniagr0808@gmail.com>
2016-09-24 15:32:44 +0300

    another bugfix

--- services/auth/passport.js
+++ services/auth/passport.js
@@ -16,7 +16,7 @@ const localLogin = new LocalStrategy(localOptions, function(email, password, don
         if (!user) { return done(null, false); }
 
         // compare passwords - is `password` equal to user.password?
-        AuthModel.comparePassword(password, function(err, isMatch) {
+        user.comparePassword(password, function(err, isMatch) {
             if (err) { return done(err); }
             if (!isMatch) { return done(null, false); }
 
